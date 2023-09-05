cd77ee4338134d4dd3d8d91cd2cc55b896c0576f
YRIBS821 <143725785+YRIBS821@users.noreply.github.com>
2023-09-05 09:46:29 -0500

    Add files via upload

---
+++ Listatareas.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package variables;
+
+/**
+ *
+ * @author labcca
+ */
+public class Listatareas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int Tareas = 5;
+        String tarea1 = "mate";
+        String tarea2 = "tics";
+        String tarea3 = "fundamentos de investigacion";
+        String tarea4 ="Fundamentos de programacion";
+        String tarea5 ="etica";
+      final int tareafinal;
+      
+      System.out.println ("La tarea 1 es de " + tarea1 );
+      System.out.println ("La tarea 2 es de: " + tarea2 );
+      System.out.println ("La tarea 3 es de: " + tarea3 );
+      System.out.println ("La tarea 4 es de: " + tarea4 );
+      System.out.println ("La tarea 5 es de: " + tarea5 );
+    }
+    
+}
---
+++ Yuliana2.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package variables;
+
+/**
+ *
+ * @author labcca
+ */
+public class Yuliana2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int edad = 0;
+        String nombre = "yuli";
+        String carrera = "tics";
+        String nombreMascota = "tortuguita";
+        String nombreMateria ="Fundamentos de programacion";
+      final int anioNacimiento =2005;
+      edad = 18;
+      System.out.println ("Mi nombre es: " + nombre + "y estudio " + carrera);
+      System.out.println ("mi materia favorita es: " + nombreMateria );
+      System.out.println ("mi mascota favorita es: " + nombreMascota );
+      System.out.println ("mi anio de nacimiento es: " + anioNacimiento );
+      System.out.println ("mi edad es: " + edad );
+     
+    }
+    }
+    
+
